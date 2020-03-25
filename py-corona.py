59236ff9855b74100ef2d3c4d77492558c7c6e23
salida <salidasbp@hotmail.com>
2020-03-25 19:48:02 +0200

    Total rewrite of the application, added functionality

--- py-corona.py
+++ py-corona.py
@@ -1,41 +1,92 @@
+import argparse
 import datetime
 import requests
+import sys
 import time
-import os
-from datetime import timedelta
+from dateutil import parser
 
-URL = "https://services9.arcgis.com/N9p5hsImWXAccRNI/arcgis/rest/services/Z7biAeD8PAkqgmWhxG2A/FeatureServer/1/query?f=json&where=(Confirmed%20%3E%200)%20AND%20(Country_Region%3D%27Greece%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc%2CCountry_Region%20asc%2CProvince_State%20asc&outSR=102100&resultOffset=0&resultRecordCount=250&cacheHint=true"
+URL = "https://bing.com/covid/data?IG=7C3AD603E5764F8585272A649F2A0E5B"
 
 
-def time_setup(datatime):
-    datetimeFormat = "%m/%d/%Y %H:%M:%S"
+def time_setup(datatime, iso=False):
+    datetimeFormat = "%Y-%m-%d %H:%M:%S"
     time_now = time.strftime(datetimeFormat, time.localtime())
-    time_last_update = time.strftime(datetimeFormat, time.localtime(datatime / 1000.0))
+
+    if not iso:
+        time_last_update = time.strftime(datetimeFormat, time.localtime(datatime / 1000.0))
+    else:
+        datatime=datatime[:19]
+        time_last_update = parser.isoparse(datatime)
+
     time_diff = datetime.datetime.strptime(
         time_now, datetimeFormat
-    ) - datetime.datetime.strptime(time_last_update, datetimeFormat)
-    print("time now    : ", time_now)
-    print("last update : ", time_last_update)
+    ) - datetime.datetime.strptime(str(time_last_update), datetimeFormat)
+
+    print("Time Now         : {} \nLast Update      : {} \nTime DIFF        : {}".format(time_now, time_last_update, time_diff))
 
 
-def get_info(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        data = data["features"][0]["attributes"]
-        time_setup(data["Last_Update"])
+def get_info(url, country):
+    headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        }
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        sys.exit("ERROR : {} : {}".format(response.status_code, URL))
+    data = response.json()
+    data = data['areas']
+    coun=[]
+    for item in range(len(data)):
+        if country in data[item]['id']:
+            country_data = data[item]
+
+    time_setup(country_data['lastUpdated'], iso=True)
+    return country_data
+
+
+def all_countries(country=None):
+    countries = ['chinamainland', 'italy', 'unitedstates', 'spain', 'germany', 'iran', 'france',
+        'southkorea', 'switzerland', 'unitedkingdom', 'netherlands', 'austria', 'belgium', 'norway',
+        'sweden', 'denmark', 'canada', 'portugal', 'malaysia', 'brazil', 'australia', 'japan',
+        'czechrepublic', 'turkey', 'israel', 'ireland', 'pakistan', 'diamondprincess', 'luxembourg',
+        'chile', 'poland', 'ecuador', 'greece', 'finland', 'qatar', 'iceland', 'indonesia', 'singapore',
+        'thailand', 'saudiarabia', 'slovenia', 'philippines', 'romania', 'peru', 'india', 'bahrain',
+        'russia', 'estonia', 'egypt', 'hongkong', 'panama', 'southafrica', 'lebanon', 'argentina', 'iraq',
+        'colombia', 'croatia', 'mexico', 'slovakia', 'kuwait', 'serbia', 'bulgaria', 'armenia',
+        'sanmarino', 'taiwan', 'unitedarabemirates', 'algeria', 'latvia', 'costarica', 'dominicanrepublic',
+        'uruguay', 'hungary', 'jordan', 'morocco', 'bosniaandherzegovina', 'vietnam', 'andorra',
+        'northmacedonia', 'brunei', 'moldova', 'albania', 'belarus', 'cyprus', 'malta', 'srilanka',
+        'venezuela', 'lithuania', 'newzealand', 'burkinafaso', 'tunisia', 'kazakhstan', 'azerbaijan',
+        'cambodia', 'oman', 'georgia', 'palestinianauthority', 'ukraine', 'senegal', 'uzbekistan',
+        'liechtenstein', 'cameroon', 'bangladesh', 'afghanistan', 'honduras', 'congodrc', 'nigeria',
+        'paraguay', 'cuba', 'ghana', 'kosovo', 'bolivia', 'jamaica', 'guatemala', 'guernsey', 'macau',
+        'rwanda', 'togo', 'jersey', 'ctedivoire', 'kyrgyzstan', 'mauritius', 'montenegro', 'maldives',
+        'monaco', 'mongolia', 'ethiopia', 'trinidadandtobago', 'kenya', 'seychelles', 'equatorialguinea',
+        'tanzania', 'barbados', 'bahamas', 'gabon', 'guyana', 'suriname', 'capeverde',
+        'centralafricanrepublic', 'elsalvador', 'madagascar', 'liberia', 'namibia', 'zimbabwe', 'angola',
+        'benin', 'bhutan', 'fiji', 'haiti', 'isleofman', 'mauritania', 'nicaragua', 'saintlucia', 'sudan',
+        'zambia', 'antiguaandbarbuda', 'chad', 'djibouti', 'eritrea', 'eswatini', 'guinea', 'nepal', 'niger',
+        'papuanewguinea', 'stvincentandthegrenadines', 'somalia', 'uganda', 'vaticancity']
+    if country in countries:
+        return country
     else:
-        data = None
-        print(response.status_code)
-    return data
-
+        print("INFO : available countries :\n", countries)
+        sys.exit("ERROR : finding country {} in available countries".format(country))
 
 def main():
-    data = get_info(URL)
-    if data:
-        items = ["Country_Region", "Confirmed", "Active", "Recovered", "Deaths"]
-        for item in items:
-            print(item, data[item])
+    parser = argparse.ArgumentParser(
+    description="""search statistics for corona virus in bing Example Usage:
+    python py-corona.py --c greece"""
+    )
+    parser.add_argument("--country", metavar="--c", type=str, default='greece', help="enter country name")
+    args = parser.parse_args()
+    country = args.country.lower()
+    all_countries(country)
+
+    data = get_info(URL, country)
+
+    items = ['displayName', 'totalConfirmed', 'totalRecovered', 'totalDeaths']
+    for i in items:
+        print ('{} : '.format(i), data[i])
 
 
 main()
